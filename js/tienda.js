885dc7ea4f595551de10353be48c69325121d43f
Roberto Camargo <rcgarcia@outlook.com>
2015-12-11 13:24:13 -0600

    Grid con hover

--- js/tienda.js
+++ js/tienda.js
@@ -1,1 +1,15 @@
-document.addEventListener("touchstart", function(){}, true);
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
+var target = null;
+$('.img_thumb').hover(function(e){
+    target = $(this);
+    $(target[0].firstElementChild).fadeIn(200);
+}, function(){
+    $(target[0].firstElementChild).fadeOut(200);
+});
