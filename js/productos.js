ef47ef4f4c98894a18c176f06fbf75fb61da015b
Marcia He <marcia@kubik.mx>
2015-12-11 01:52:34 -0600

    json

--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,19 @@
 var myDataRef = new Firebase('https://tunovendespinas.firebaseio.com/');
 
-myDataRef.child(3).on("precio", function(snapshot) {
-  alert(snapshot.val());  
+myDataRef.on("value", function(snapshot) {
+	//console.log(snapshot)
+  data = snapshot.val();
+  
+  productos_ropa = snapshot.child("categorias/ropa/productos").val();
+
+  productos_ropa.forEach(function(producto){
+  	if(producto.descripcion == "calcetines Piña"){
+  		$("#myModalLabel").html(producto.descripcion);
+  		console.log($("#myModalLabel"))
+  		console.log(producto.descripcion)
+  		$(".detalle-precio h3").text("$ " + producto.precio);
+  		console.log("RUJNNNN")
+  	}
+
+  });
 });
