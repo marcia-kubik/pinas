f37be541c3c8317e8932ff78b320d33abeb7f11b
Marcia He <marcia@kubik.mx>
2015-12-10 21:17:36 -0600

    Avance proyectos

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,5 @@
 var myDataRef = new Firebase('https://tunovendespinas.firebaseio.com/');
 
-$('#messageInput').keypress(function (e) {
-        if (e.keyCode == 13) {
-          var name = $('#nameInput').val();
-          var text = $('#messageInput').val();
-          myDataRef.set('User ' + name + ' says ' + text);
-          $('#messageInput').val('');
-        }
-      });
+myDataRef.child(3).on("precio", function(snapshot) {
+  alert(snapshot.val());  
+});
