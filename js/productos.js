82958e53dacf7ba69ce43f59f92dbb37f8594ee8
Marcia He <marcia@kubik.mx>
2015-12-12 10:47:44 -0600

    Tienda y ajustes

--- js/productos.js
+++ js/productos.js
@@ -4,20 +4,29 @@ myDataRef.on("value", function(snapshot) {
   var data = snapshot.val();
   // productos_ropa = snapshot.child("categorias/ropa/productos").val();
 
-  var productos = data.categorias.accesorios.productos
+  var productos_accesorios = data.categorias.accesorios.productos
+  var productos_ropa = data.categorias.ropa.productos
+  var productos_comida = data.categorias.comida.productos
+  var productos_decoracion = data.categorias.decoracion.productos
+
+  var productos = productos_decoracion.concat(productos_ropa).concat(productos_comida).concat(productos_accesorios)
+  console.log(productos.length)
+
   var template = $('.thumb');
   var grid = $('#grid');
 
   var counter_id = 0;
-  	//grid.masonry({itemSelector:'.img_thumb'})
 
   productos.forEach(function(producto){
   	var item = template.clone();
   	// console.log("trabajando con" + producto.nombre)
   	item.removeClass('template');
   	item.attr('id', 'item' + counter_id++);
-  	item.find(".grid-nombre").text(producto.nombre)
-  	item.find('.grid-imagen').attr('src', producto.imagen)
+  	item.find(".grid-nombre").text(producto.nombre);
+  	item.find('.img_desc').attr('data-nombre', producto.nombre);
+  	item.find('.grid-imagen').attr('src', producto.imagen);
+  	item.find('.grid-precio').text(producto.precio);
+
   	grid.append(item);
   	
   });
@@ -28,7 +37,7 @@ myDataRef.on("value", function(snapshot) {
 
   function cargar_detalle(productos){
   	productos.forEach(function(producto){
-  	if(producto.nombre == "Calcetines Piña"){
+  	if(producto.nombre == 'tacos' ){
   		$("#nombre").text(producto.nombre);
   		$("#descripcion").text(producto.descripcion);
   		$("#precio").append(producto.precio+'.<sup>00<sup>')
